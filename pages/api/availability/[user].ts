ai-code-review-evaluation/cal.com-copilot

commit 30a71842031c8d252adfda370e72c7c2f93b3144

Temporarily disable checking video busy times

pages/api/availability/[user].ts
@@ -31,12 +31,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     req.query.dateTo,
     selectedCalendars
   );
-  const videoBusyTimes = await getBusyVideoTimes(
-    currentUser.credentials,
-    req.query.dateFrom,
-    req.query.dateTo
-  );
-  calendarBusyTimes.push(...videoBusyTimes);
+  // const videoBusyTimes = await getBusyVideoTimes(
+  //   currentUser.credentials,
+  //   req.query.dateFrom,
+  //   req.query.dateTo
+  // );
+  // calendarBusyTimes.push(...videoBusyTimes);
 
   const bufferedBusyTimes = calendarBusyTimes.map((a) => ({
     start: dayjs(a.start).subtract(currentUser.bufferTime, "minute").toString(),

commit 48f82ee103b3c5e27e78ba39888e6c0b3fea1021

fix linting issue introduced in 30a7184 (#529)

pages/api/availability/[user].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@lib/prisma";
 import { getBusyCalendarTimes } from "@lib/calendarClient";
-import { getBusyVideoTimes } from "@lib/videoClient";
+// import { getBusyVideoTimes } from "@lib/videoClient";
 import dayjs from "dayjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {